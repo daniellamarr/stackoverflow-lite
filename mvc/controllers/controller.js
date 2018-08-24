d23a34faa0ff2076a0302310bcc634d6e9dd7350
daniellamarr <danielchidiebele@gmail.com>
2018-08-24 02:00:13 +0100

    [Feature #159977222] Add endpoint to ask question

--- mvc/controllers/controller.js
+++ mvc/controllers/controller.js
@@ -9,6 +9,24 @@ class Controller {
         if (!question) res.status(404).send('This question may have been deleted');
         res.send(question);
     }
+    askQuestion = (req, res) => {
+        if (!req.body.title || req.body.title.length < 3) {
+            res.status(400).send('Please add the title for your question');
+            return;
+        }
+        if (!req.body.body || req.body.title.body < 3) {
+            res.status(400).send('Please add content to your question');
+            return;
+        }
+    
+        const question = {
+            id: questions.length + 1,
+            title: req.body.title,
+            body: req.body.body
+        };
+        questions.push(question);
+        res.send(question);
+    }
 }
 
 export default new Controller;
