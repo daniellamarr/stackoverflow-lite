bee9d391e628c4a7f16a871c49bd4344445ff9ce
daniellamarr <danielchidiebele@gmail.com>
2018-08-28 10:11:43 +0100

    [Feature #160087760] Add route for signup and login

--- mvc/routes/route.js
+++ mvc/routes/route.js
@@ -4,11 +4,13 @@ const Route = (app) => {
 
     app.get('/api/v1/questions', Controller.getAllQuestions);
 
-    app.get('/api/v1/questions/:id', Controller.getOneQuestion)
+    app.get('/api/v1/questions/:id', Controller.getOneQuestion);
 
-    app.post('/api/v1/questions', Controller.askQuestion)
+    app.post('/api/v1/questions', Controller.askQuestion);
 
-    app.post('/api/v1/questions/:id/answers', Controller.postAnswer)
+    app.post('/api/v1/questions/:id/answers', Controller.postAnswer);
+
+    app.post('/api/v1/auth/signup', Controller.userSignup);
 
 }
 
