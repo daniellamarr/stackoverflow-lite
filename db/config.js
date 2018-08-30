956bd56ea01055dae16fe6f458b42894499564bb
daniellamarr <danielchidiebele@gmail.com>
2018-08-30 17:24:30 +0100

    [Chore #159988069] Deploy app to heroku

--- db/config.js
+++ db/config.js
@@ -2,11 +2,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// const config = {
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASS
+// };
+
 const config = {
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS
-};
+    connectionString: process.env.DATABASE_URL
+}
 
 export default config;
