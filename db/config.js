011af814a216f06387ae5d62ccebb06f2fe531eb
daniellamarr <danielchidiebele@gmail.com>
2018-08-31 09:00:27 +0100

    [Chore #159988069] App deploy test

--- db/config.js
+++ db/config.js
@@ -8,13 +8,15 @@ const config = {
         user: process.env.DB_USER,
         host: process.env.DB_HOST,
         database: process.env.DB_NAME,
-        password: process.env.DB_PASS
+        password: process.env.DB_PASS,
+        port: process.env.DB_PORT
     },
     test: {
         user: process.env.DB_USER,
         host: process.env.DB_HOST,
         database: process.env.TEST_DB_NAME,
-        password: process.env.DB_PASS
+        password: process.env.DB_PASS,
+        port: process.env.DB_PORT
     }
 }
 
--- db/index.js
+++ db/index.js
@@ -1,12 +1,13 @@
 import { Pool } from "pg";
 import dotenv from "dotenv";
 import config from "./config";
+import parseUrl from "parse-database-url";
 
 dotenv.config();
 let mode;
 
 if (process.env.NODE_ENV === "production") {
-    mode = process.env.DATABASE_URL;
+    mode = parseUrl(process.env.DATABASE_URL);
 }else{
     mode = config[process.env.NODE_ENV || "development"];
 }
